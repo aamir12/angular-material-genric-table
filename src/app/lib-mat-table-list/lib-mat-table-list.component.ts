7743d32c956fa1d9b1bf8a560be073efbd7a1dc2
Aamir Khan <aamirkhan8878@gmail.com>
2024-12-21 09:14:10 +0530

    Update lib-mat-table-list.component.ts

--- src/app/lib-mat-table-list/lib-mat-table-list.component.ts
+++ src/app/lib-mat-table-list/lib-mat-table-list.component.ts
@@ -3,20 +3,17 @@ import {
   Inject,
   Input,
   OnInit,
-  Output,
   SimpleChanges,
   ViewChild,
-  EventEmitter
 } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { AfterRenderData, IActionBtnConfiguration, IColumn } from '../model';
+import { IActionBtnConfiguration, IColumn } from '../model';
 import { PageEvent } from '@angular/material/paginator';
 import { uniqueId } from '../utility/common.fn';
 import { DOCUMENT } from '@angular/common';
 import { WINDOW } from '../window.service';
-
 
 @Component({
   selector: 'lib-mat-table-list',
@@ -25,7 +22,7 @@ import { WINDOW } from '../window.service';
 })
 export class LibMatTableListComponent<T> implements OnInit {
   @Input() data: T[] = [];
-  @Input() filterValue: string = '';
+  @Input() filterValue: string;
   @Input() containerClasses: string[] = [];
   @Input() tableContainerClasses: string[] = [];
   @Input() paginationClasses: string[] = [];
@@ -37,22 +34,20 @@ export class LibMatTableListComponent<T> implements OnInit {
   @Input() sortFn!: (data: T[], sort: MatSort) => T[];
   @Input() sortActive: string = '';
   @Input() sortDirection: 'asc' | 'desc' | '' = 'asc';
-  @Input() actionBtns: IActionBtnConfiguration<T> | undefined = undefined;
-  @Input() isLoading: boolean = false;
-  @Input() loadingLable: string = 'Loading...';
-  @Input() noDataFoundLable: string = 'No Data Found.';
-  @Input() isScrollUpAfterPageChange: boolean = true;
-  @Output() afterTableRender = new EventEmitter<AfterRenderData>();
+  @Input() actionBtns!: IActionBtnConfiguration<T>;
+  @Input() isPageAble: boolean = true;
+
   dataSource!: MatTableDataSource<T>;
-  @ViewChild(MatPaginator,{static:true}) paginator!: MatPaginator;
-  @ViewChild(MatSort,{static:true}) sort!: MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort!: MatSort;
   displayedColumns: string[] = [];
   tableContainerId = uniqueId();
-
+  dataCount: number = 0;
+  pageIndex: number = 0;
   constructor(
     @Inject(DOCUMENT) private document: Document,
-    @Inject(WINDOW) private window: Window,
-    ) {}
+    @Inject(WINDOW) private window: Window
+  ) {}
   ngOnInit() {
     this.setUpcolumnsSetting();
     this.setUpByData();
@@ -62,8 +57,10 @@ export class LibMatTableListComponent<T> implements OnInit {
 
   setUpByData() {
     this.dataSource = new MatTableDataSource(this.data);
-    this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+    if (this.isPageAble) {
+      this.dataSource.paginator = this.paginator;
+    }
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -75,8 +72,7 @@ export class LibMatTableListComponent<T> implements OnInit {
       changes['data'] &&
       changes['data'].currentValue !== changes['data'].previousValue
     ) {
-      console.log('Data Changed');
-      this.setUpByData()
+      this.setUpByData();
       this.filterDefinition();
       this.sortDefinition();
       this.applyFilter();
@@ -89,6 +85,12 @@ export class LibMatTableListComponent<T> implements OnInit {
         changes['filterValue'].previousValue
     ) {
       this.applyFilter();
+    }
+
+    if (changes['isPageAble'] && changes['isPageAble'].currentValue) {
+      this.dataSource.paginator = this.paginator;
+    } else if (changes['isPageAble'] && !changes['isPageAble'].currentValue) {
+      this.dataSource.paginator = null;
     }
   }
 
@@ -117,7 +119,7 @@ export class LibMatTableListComponent<T> implements OnInit {
   }
 
   applyFilter() {
-    if (this.filterValue.trim()) {
+    if (this.filterValue?.trim()) {
       this.dataSource.filter = this.filterValue.trim();
 
       if (this.dataSource.paginator) {
@@ -125,7 +127,6 @@ export class LibMatTableListComponent<T> implements OnInit {
       }
     }
   }
-
 
   onRowClick(row: T) {
     if (this.rowClickListner) {
@@ -138,30 +139,15 @@ export class LibMatTableListComponent<T> implements OnInit {
   }
 
   onPageChange(event: PageEvent) {
-    if(!this.isScrollUpAfterPageChange ) {
-      return;
-    }
     const tableContainer = this.document.getElementById(this.tableContainerId);
     this.window.scrollTo(0, (tableContainer as HTMLElement).offsetTop - 30);
-    //(tableContainer as HTMLElement).scrollIntoView();
-
- 
   }
 
-  isActionSticky(position:'start'|'end'):boolean {
-    return  this.dataSource?.filteredData.length > 0 && this.actionBtns?.positions === position && !!this.actionBtns?.sticky;
-  }
-
-  onContentRendered() {
-    this.afterTableRender.emit(
-      {
-        renderedRows: this.dataSource.filteredData.length || 0,
-        reRenderTableFn: this.reRenderTable.bind(this)
-      }
-      )
-  }
-
-  reRenderTable() {
-    this.dataSource._updateChangeSubscription();
+  isActionSticky(position: 'start' | 'end'): boolean {
+    return (
+      this.dataSource?.filteredData.length > 0 &&
+      this.actionBtns?.positions === position &&
+      !!this.actionBtns?.sticky
+    );
   }
 }
